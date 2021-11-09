e749248225532342cce7c5ce7871cf6214032456
itzrizvi <shahriar.me.work@gmail.com>
2021-11-09 21:34:11 +0600

    date changes

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ async function run() {
         // All APPOINTMENT GET
         app.get('/appointments', verfiyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             const query = { email: email, date: date };
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
